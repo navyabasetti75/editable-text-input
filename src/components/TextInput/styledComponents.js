17b5dfc917eb31a729c01e201790394ba069f0d8
navya <navyabasetti75@gmail.com>
2023-07-15 11:32:18 +0000

    editable text input

---
+++ src/components/TextInput/styledComponents.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  background-color: #f5d0fe;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  padding: 30px;
+  border-radius: 10px;
+  height: 200px;
+`
+export const Heading = styled.h1`
+  font-size: 20px;
+  font-family: 'Roboto';
+`
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+export const Paragraph = styled.p`
+  font-size: 15px;
+  font-family: 'Roboto';
+  padding-right: 10px;
+`
+export const Input = styled.input`
+  height: 30px;
+  width: 250px;
+  outline: none;
+  margin-right: 20px;
+  padding: 10px;
+`
+export const Button = styled.button`
+  height: 30px;
+  width: 55px;
+  border-width: 0;
+  font-size: 13px;
+  font-family: 'Roboto';
+  color: white;
+  cursor: pointer;
+  background-color: #d946ef;
+`
